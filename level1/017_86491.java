a83935d52b43bda6ac9286f92c3c6bb7575161ad
김세영 <48676248+KimBaek-Seyeong@users.noreply.github.com>
2023-02-19 21:11:10 +0900

    Create 017_86491.java

---
+++ level1/017_86491.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        int maxX = 0, maxY = 0;
+        for(int i=0; i<sizes.length; i++){
+            if(sizes[i][0] < sizes[i][1]){
+                int tmp = sizes[i][0];
+                sizes[i][0] = sizes[i][1];
+                sizes[i][1] = tmp;
+            }
+            
+            if(sizes[i][0] > maxX){
+                maxX = sizes[i][0];
+            }
+            
+            if(sizes[i][1] > maxY){
+                maxY = sizes[i][1];
+            }
+        }
+            
+        answer = maxX*maxY;
+        return answer;
+    }
+}
